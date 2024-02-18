ab9d746a250e019401756621aedfd4f0691a447a
andrew <andrewliu278@gmail.com>
2024-02-17 22:16:38 -0800

    animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
   return (
     <main className={classes.main}>
       <div className={`${classes.flex} ${classes.titleContainer}`}>
-        <div className={classes.title}>
+        <div className={`${classes.title} ${classes.animate}`}>
           Talk to the Terminal{" "}
           <div className={classes.gradientText}>in English</div>
         </div>
-        <div className={classes.subTitle}>
-          Natural language queries to supercharge your development workflow.
+        <div className={`${classes.subTitle} ${classes.animate}`}>
+          Natural language queries supercharge your development workflow.
           Powered by offline, safe and secure AI.
         </div>
       </div>
