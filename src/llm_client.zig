c21b6e7aed0dfd783fcae1a4c689145e13d05c83
Albert Tam <tamalbert8@gmail.com>
2024-02-17 22:19:17 -0800

    openai

--- src/llm_client.zig
+++ src/llm_client.zig
@@ -1,9 +1,7 @@
 const std = @import("std");
 const config = @import("config.zig");
 
-// const request_uri = "https://godsays.xyz/";
-
-const Message = struct {
+pub const Message = struct {
     role: []const u8,
     content: []const u8,
 };
@@ -37,11 +35,52 @@ const system_message = Message{
     .content = "You are a helpful assistant.",
 };
 
+pub fn sendOpenaiRequest(allocator: std.mem.Allocator, messageHistory: []const Message) ![]const u8 {
+    var client = std.http.Client{ .allocator = allocator };
+    defer client.deinit();
+
+    const uri = std.Uri.parse(config.OPENAI_DOMAIN) catch unreachable;
+
+    const requestBody = RequestBody{
+        .model = "gpt-3.5-turbo",
+        .messages = messageHistory,
+    };
+
+    var headers = std.http.Headers{ .allocator = allocator };
+    defer headers.deinit();
+
+    try headers.append("Content-Type", "application/json");
+    try headers.append("Authorization", "Bearer " ++ config.API_KEY);
+
+    var request = try client.request(.POST, uri, headers, .{});
+    defer request.deinit();
+    request.transfer_encoding = .chunked;
+
+    try request.start();
+    try std.json.stringify(requestBody, .{}, request.writer());
+
+    try request.finish();
+    try request.wait();
+
+    var body = std.ArrayList(u8).init(allocator);
+    defer body.deinit();
+    try request.reader().readAllArrayList(&body, 40960);
+
+    // std.debug.print("response: {s}\n", .{body});
+
+    const parsed_json = try std.json.parseFromSlice(OpenAIResponseBody, allocator, body.items, .{});
+    defer parsed_json.deinit();
+
+    const response_body = parsed_json.value;
+
+    return allocator.dupe(u8, response_body.choices[0].message.content);
+}
+
 pub fn sendRequest(allocator: std.mem.Allocator, userMessage: []u8) ![]const u8 {
     var client = std.http.Client{ .allocator = allocator };
     defer client.deinit();
 
-    const uri = std.Uri.parse(config.DOMAIN) catch unreachable;
+    const uri = std.Uri.parse(config.OPENAI_DOMAIN) catch unreachable;
     // const uri = std.Uri.parse("http://127.0.0.1:5000") catch unreachable;
 
     const messages = [_]Message{
@@ -53,16 +92,6 @@ pub fn sendRequest(allocator: std.mem.Allocator, userMessage: []u8) ![]const u8 
         .model = "gpt-3.5-turbo",
         .messages = &messages,
     };
-
-    // var buf: [16384]u8 = undefined;
-    // var fba = std.heap.FixedBufferAllocator.init(&buf);
-    // const fba_allocator = fba.allocator();
-
-    // try std.json.stringify(requestBody, .{}, std.io.getStdOut().writer());
-    // const jsonString = try std.json.stringifyAlloc(fba_allocator, requestBody, .{});
-    // defer fba_allocator.free(jsonString);
-
-    // std.debug.print("Serialized JSON: {s}\n", .{jsonString});
 
     var headers = std.http.Headers{ .allocator = allocator };
     defer headers.deinit();
@@ -83,7 +112,7 @@ pub fn sendRequest(allocator: std.mem.Allocator, userMessage: []u8) ![]const u8 
     const body = request.reader().readAllAlloc(allocator, 40960) catch unreachable;
     defer allocator.free(body);
 
-    std.debug.print("response: {s}\n", .{body});
+    // std.debug.print("response: {s}\n", .{body});
 
     const parsed_json = std.json.parseFromSlice(OpenAIResponseBody, allocator, body, .{}) catch unreachable;
     defer parsed_json.deinit();
