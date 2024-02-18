d4f7460c51c8adb70b1f043dfca1c3286dd442bb
andrew <andrewliu278@gmail.com>
2024-02-17 23:09:23 -0800

    github button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+"use client";
+
 import classes from "./page.module.css";
 
 export default function Home() {
@@ -10,9 +11,36 @@ export default function Home() {
           <div className={classes.gradientText}>in English</div>
         </div>
         <div className={`${classes.subTitle} ${classes.animate}`}>
-          Natural language queries supercharge your development workflow.
+          Natural language queries to supercharge your development workflow.
           Powered by offline, safe and secure AI.
         </div>
+        <div className={`${classes.buttonContainer} ${classes.animate}`}>
+          <button
+            onClick={() => {
+              console.log("github");
+            }}
+            className={classes.githubButton}
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <img
+              src="/images/github.svg"
+              alt="GitHub"
+              style={{
+                width: "30px",
+                background: "white",
+              }}
+            />
+            <div className={`${classes.black}`}>GitHub</div>
+          </button>
+        </div>
+        <img
+          src={"/images/terminal.png"}
+          alt="terminal"
+          className={`${classes.terminal} ${classes.animate}`}
+        />
       </div>
     </main>
   );
