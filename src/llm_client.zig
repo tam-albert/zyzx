18f7969fdf1ea5a58caae3f166c5d348b01f89e4
Albert Tam <tamalbert8@gmail.com>
2024-02-18 00:20:16 -0800

    little changes

--- src/llm_client.zig
+++ src/llm_client.zig
@@ -80,51 +80,16 @@ pub fn sendOpenAIStreamingRequest(allocator: std.mem.Allocator, writer: anytype,
     try request.finish();
     try request.wait();
 
-    // var final_response_content = std.ArrayList(u8).init(allocator);
-    // defer final_response_content.deinit();
-
-    // while (true) {
-    //     var buffer: [10240]u8 = undefined;
-    //     const bytes_read = try request.read(buffer[0..]);
-    //     if (bytes_read == 0) break; // End of stream
-
-    //     const event = buffer[0..bytes_read];
-
-    //     std.debug.print("Received event: {s}\n", .{event});
-
-    //     const start = std.mem.indexOf(u8, event, "data: ");
-    //     if (start) |idx| {
-    //         const trueStart = idx + "data: ".len;
-    //         const end = std.mem.indexOf(u8, event, "\n\n") orelse break;
-
-    //         // std.debug.print("Parsing JSON: {s}\n", .{event[trueStart..end]});
-
-    //         // parse the event, and append the content object to the response_content
-    //         const parsed_json = try std.json.parseFromSlice(OpenAIStreamingResponseBody, allocator, event[trueStart..end], .{});
-    //         defer parsed_json.deinit();
-
-    //         const response_chunk = parsed_json.value.choices[0].delta.content;
-    //         std.debug.print("Chunk {d}: {s}\n", .{ i, response_chunk });
-    //         // try writer.writeAll(response_chunk);
-    //     } else {
-    //         break;
-    //     }
-
-    //     i += 1;
-    // }
-
-    var i: u32 = 0;
-
     while (true) {
         var buffer: [10240]u8 = undefined;
         const bytes_read = try request.read(buffer[0..]);
-        if (bytes_read == 0) break; // End of stream
+        if (bytes_read == 0) break;
 
-        var event_start: usize = 0; // Start of the current event in buffer
+        var event_start: usize = 0;
 
         // std.debug.print("RECEIVED CHUNK\n---\n{s}\n---\n", .{buffer[0..bytes_read]});
         while (event_start < bytes_read) {
-            const chunk = buffer[event_start..bytes_read]; // Consider the remaining part of the buffer
+            const chunk = buffer[event_start..bytes_read];
             if (std.mem.eql(u8, chunk, "data: [DONE]\n\n")) {
                 break;
             }
@@ -138,7 +103,6 @@ pub fn sendOpenAIStreamingRequest(allocator: std.mem.Allocator, writer: anytype,
                     const event_data = buffer[event_data_start..event_end];
                     // std.debug.print("Parsing JSON: {s}\n\n", .{event_data});
 
-                    // Parse the JSON event
                     const parsed_json = try std.json.parseFromSlice(OpenAIStreamingResponseBody, allocator, event_data, .{});
                     defer parsed_json.deinit();
 
@@ -146,21 +110,15 @@ pub fn sendOpenAIStreamingRequest(allocator: std.mem.Allocator, writer: anytype,
                     try writer.writeAll(response_chunk);
                     // std.debug.print("Chunk {d}: {s}\n", .{ i, response_chunk });
 
-                    // Move offset to the end of the current event, plus the length of "\n\n"
                     event_start = event_end + "\n\n".len;
                 } else {
-                    // If the end of the event is not within this chunk, wait for more data
                     break;
                 }
             } else {
-                // If no start is found, wait for more data
                 break;
             }
-            i += 1; // Increment counter for each event processed
         }
     }
-
-    // return final_response_content.toOwnedSlice();
 }
 
 pub fn sendOpenaiRequest(allocator: std.mem.Allocator, messageHistory: []const Message) ![]const u8 {
@@ -215,28 +173,3 @@ pub fn strip_response(allocator: std.mem.Allocator, userMessage: []u8) ![]const 
     // return CMD;
     return allocator.dupe(u8, res);
 }
-
-// pub fn sendGetRequest(allocator: std.mem.Allocator) !void {
-//     var client = std.http.Client{ .allocator = allocator };
-//     defer client.deinit();
-
-//     const uri = std.Uri.parse("https://godsays.xyz/") catch unreachable;
-
-//     var headers = std.http.Headers{ .allocator = allocator };
-//     defer headers.deinit();
-
-//     try headers.append("Accept", "*/*");
-
-//     var request = try client.request(.GET, uri, headers, .{});
-//     defer request.deinit();
-
-//     try request.start();
-//     try request.finish();
-
-//     try request.wait();
-
-//     const body = request.reader().readAllAlloc(allocator, 32768) catch unreachable;
-//     defer allocator.free(body);
-
-//     std.log.info("response: {s}", .{body});
-// }
